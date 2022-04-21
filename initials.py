690f8834d304c3babfb84ccb4e22c6841972f7eb
Warren Spalding <w.spalding.toc@gmail.com>
2022-04-21 21:06:58 +0100

    added initials.py

---
+++ initials.py
@@ -0,0 +1,11 @@
+# Warren Spalding
+# Fun Fact:  My favourite sweet is caramel
+
+#      12345  12345
+print("W   W   SSS ")
+print("W   W  S   S")
+print("W   W  S    ")
+print("W W W   SSS ")
+print("WW WW      S")
+print("WW WW  S   S")
+print("W   W   SSS ")
